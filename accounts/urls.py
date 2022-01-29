5f55ea32cfa726ee515a41ee662da29346fb5ad6
XXmmRR <Xorex50@protonmail.com>
2022-01-29 18:48:56 +0500

    replace and register model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,7 @@
-#from .views import SignUpView
-#from django.urls import path
-#from .views import post_detail
+from .views import SignUpView
+from django.urls import path
 
-#urlpatterns = [
-#    path('signup/', SignUpView.as_view(), name='signup'),
-#    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-#         views.post_detail, name='post_detail')
-#]
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
 
