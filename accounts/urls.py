070dbf79f68f726d46983ae70906717fea2747f0
XXmmRR <Xorex50@protonmail.com>
2022-01-29 18:40:27 +0500

    add blog model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
-from .views import SignUpView
-from django.urls import path
+#from .views import SignUpView
+#from django.urls import path
+#from .views import post_detail
 
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
+#urlpatterns = [
+#    path('signup/', SignUpView.as_view(), name='signup'),
+#    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+#         views.post_detail, name='post_detail')
+#]
 
