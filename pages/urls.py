1c3db6fc3faaaba5379fe31f03e7a0abd02f948c
XXmmRR <Xorex50@protonmail.com>
2022-01-29 17:14:04 +0500

    add contact page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 from django.urls import path
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('contact/', AboutPageView.as_view(), name='contact')
 ]
