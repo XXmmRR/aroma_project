54ad8bb9fbc816de78ed6b4043d1962e4950f67f
XXmmRR <Xorex50@protonmail.com>
2022-02-02 22:30:49 +0500

    add model Comment

--- blog/models.py
+++ blog/models.py
@@ -43,3 +43,19 @@ class BlogModel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogModel, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
