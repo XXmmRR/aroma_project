b30054476458465e60bad198f56f71998287858e
XXmmRR <Xorex50@protonmail.com>
2022-02-12 21:53:24 +0500

    _

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Product, Category
+from django.db.models import Count
 
 # Create your views here.
 
 
-class ShopView(TemplateView):
+class ShopView(ListView):
     template_name = 'shop/category.html'
+    model = Product
+    queryset = Product.objects.annotate(num_products=Count('product_type')).all()
+    extra_context = {'categories': Category.objects.all()}
+    context_object_name = 'products'
