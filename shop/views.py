6013b48f2947be49b6477adab7d72c74ca564d44
XXmmRR <Xorex50@protonmail.com>
2022-02-14 21:32:09 +0500

    add another paginator

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product, Category
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -18,19 +19,26 @@ class ShopView(ListView):
 def shop_category(request, slug):
     posts = Product.objects.filter(category__slug=slug)
     menu = Category.objects.all()
-    return render(request, 'shop/category.html', {'products': posts, 'categories': menu})
+    paginator = Paginator(posts, 12)  # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'shop/category.html', {'products': posts, 'categories': menu, 'page_obj': page_obj})
 
 
 class ShopSearchResultsListView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'shop/category.html'
+    paginate_by = 12
 
     def get_queryset(self):  # new
         query = self.request.GET.get('q')
         return Product.objects.filter(
             Q(title__icontains=query)
         )
+
 
 class ShopDetailView(DetailView):
     model = Product
