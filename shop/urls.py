1fb3a82bb5fc4b6f5e2065787b24ddf63920ec7a
XXmmRR <Xorex50@protonmail.com>
2022-02-14 20:33:21 +0500

    add search system

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import ShopView, ShopDetailView, shop_category
+from .views import ShopView, ShopDetailView, shop_category, ShopSearchResultsListView
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
     path('category/<slug:slug>/', shop_category, name='shop_category'),
+    path('search/', ShopSearchResultsListView.as_view(), name='search_results'),
     path('<int:pk>/<slug:slug>/', ShopDetailView.as_view(), name='single_shop')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product, Category
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -18,6 +20,17 @@ def shop_category(request, slug):
     menu = Category.objects.all()
     return render(request, 'shop/category.html', {'products': posts, 'categories': menu})
 
+
+class ShopSearchResultsListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'shop/category.html'
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get('q')
+        return Product.objects.filter(
+            Q(title__icontains=query)
+        )
 
 class ShopDetailView(DetailView):
     model = Product
