0094f11ed876c93ae7f9cdd81b002e3878d87901
XXmmRR <Xorex50@protonmail.com>
2022-02-28 00:33:00 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'config.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'Aroma.storage.WhiteNoiseStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
