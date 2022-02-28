d97ba2289d921217ebcc255cd3269ce95a0a673d
XXmmRR <Xorex50@protonmail.com>
2022-02-28 09:52:21 +0300

    add redirection

--- shop/views.py
+++ shop/views.py
@@ -105,7 +105,7 @@ def cart_add(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.add(product=product)
-    return redirect("home")
+    return redirect("cart_detail")
 
 
 @login_required
